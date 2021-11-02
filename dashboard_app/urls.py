0f1ab1e103fbe42ddeaf77eb82faebb837800f2f
Yevgen <yevgen.yelik@gmail.com>
2021-11-02 20:54:54 +0200

    add logout, post viewset urls

--- dashboard_app/urls.py
+++ dashboard_app/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from rest_framework import routers
 
 from dashboard_app.auth import UserRegistrationView, LoginView, LogoutView
-from dashboard_app.views import UserViewSet
+from dashboard_app.views import UserViewSet, PostViewSet
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet, basename='users')
+router.register('posts', PostViewSet, basename='posts')
 
 urlpatterns = [
-    path('register', UserRegistrationView.as_view(), name='register'),
-    path('login', LoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
+    path('auth/register', UserRegistrationView.as_view(), name='register'),
+    path('auth/login', LoginView.as_view(), name='login'),
+    path('auth/logout', LogoutView.as_view(), name='logout'),
 ]
 
 urlpatterns += router.urls
