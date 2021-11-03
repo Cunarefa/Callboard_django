30f7f11a0e1cf2b1a507905409f7d48e19d7b56b
Yevgen <yevgen.yelik@gmail.com>
2021-11-03 19:02:31 +0200

    add Like route

--- dashboard_app/urls.py
+++ dashboard_app/urls.py
@@ -2,16 +2,18 @@ from django.urls import path
 from rest_framework import routers
 
 from dashboard_app.auth import UserRegistrationView, LoginView, LogoutView
-from dashboard_app.views import UserViewSet, PostViewSet
+from dashboard_app.views import UserViewSet, PostViewSet, CommentViewSet, LikeView
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet, basename='users')
 router.register('posts', PostViewSet, basename='posts')
+router.register('comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('auth/register', UserRegistrationView.as_view(), name='register'),
     path('auth/login', LoginView.as_view(), name='login'),
     path('auth/logout', LogoutView.as_view(), name='logout'),
+    path('likes', LikeView.as_view(), name='like'),
 ]
 
 urlpatterns += router.urls
