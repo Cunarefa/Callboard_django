862b29ac276e2d5d9ffaef50537db71fffc048b3
Yevgen <yevgen.yelik@gmail.com>
2021-11-03 19:02:14 +0200

    alter for ManyToMany relations

--- dashboard_app/serializers.py
+++ dashboard_app/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-from dashboard_app.models import User, Post
+from dashboard_app.models import User, Post, Comment
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = User
-        fields = ('email', 'password')
+        fields = ('id', 'email', 'password', 'is_staff', 'is_admin',)
 
     email = serializers.CharField(
         required=True,
@@ -17,24 +19,35 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
 class UserListSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    liked_posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'email',)
+        fields = ('id', 'email', 'is_staff', 'is_admin', 'posts', 'comments', 'liked_posts',)
 
 
-class LikesSerializer(serializers.ModelSerializer):
+class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = ('id',)
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.PrimaryKeyRelatedField(read_only=True)
-    likes = LikesSerializer(read_only=True, many=True)
+    author = serializers.ReadOnlyField(read_only=True, source='author.id')
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    likes = UserListSerializer(read_only=True, many=True)
 
     class Meta:
         model = Post
         fields = '__all__'
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.id')
+    post = serializers.ReadOnlyField(source='post.id')
 
+    class Meta:
+        model = Comment
+        fields = '__all__'
